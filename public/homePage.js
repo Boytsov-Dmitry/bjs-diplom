ac92a2503a6779132c9b68b725e3c49358aa7ab3
Boytsov Dmitry <bdo5677905@yandex.ru>
2023-10-26 18:56:57 +0300

    Finally

--- public/homePage.js
+++ public/homePage.js
@@ -4,8 +4,7 @@ const logOut = new LogoutButton();
 
 logOut.action = () => {
     ApiConnector.logout(response => {
-        if(response) {
-            console.log('Выход из личного кабинета!')
+        if(response.success === true) {
             location.reload();
         };
     });
@@ -42,14 +41,68 @@ manage.addMoneyCallback = (data) => {
     ApiConnector.addMoney({currency: data.currency, amount: data.amount}, response => {
         if(response.success === true) {
             ProfileWidget.showProfile(response.data);
-            MoneyManager.setMessage(response.success, response.message = 'fffffff')
-        };
-
-        if(response.success !== true) {
-            console.log('Данных пользователя нет в базе данных!');
+            manage.setMessage(response.success, response.message = 'Баланс успешно пополнен!');
+        } else {
+            manage.setMessage(response.success, response.message = 'Ошибка пополнения баланса!');
         };
     });
 };
 
+manage.conversionMoneyCallback = (data) => {
+    console.log(data)
+    ApiConnector.convertMoney({ fromCurrency: data.fromCurrency, targetCurrency: data.targetCurrency, fromAmount: data.fromAmount }, response => {
+        if(response.success === true) {
+            ProfileWidget.showProfile(response.data);
+            manage.setMessage(response.success, response.message = 'Конвертация валты успешно произведена!');
+        } else {
+            manage.setMessage(response.success, response.message = 'Ошибка конвертации валюты!');
+        };
+    });
+};
 
-// FavoritesWidget.setMessage();
+manage.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney({ to: data.to, currency: data.currency, amount: data.amount }, response => {
+        if(response.success === true) {
+            ProfileWidget.showProfile(response.data);
+            manage.setMessage(response.success, response.message = 'Валюта успешно отправлена!');
+        } else {
+            manage.setMessage(response.success, response.message = 'Ошибка отправки валюты!');
+        };
+    });
+};
+
+const widget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response => {
+    if(response.success === true) {
+        widget.clearTable();
+        widget.fillTable(response.data);
+        manage.updateUsersList(response.data);
+    };
+});
+
+widget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites({id: data.id, name: data.name}, response => {
+        if(response.success === true) {    
+            widget.clearTable();
+            widget.fillTable(response.data);
+            manage.updateUsersList(response.data);
+            manage.setMessage(response.success, response.message = 'Пользователь успешно добавлен в список избранных!');
+        } else {
+            manage.setMessage(response.success, response.message = 'Ошибка при добалении пользователя в список избранных!');
+        };
+    });
+};
+
+widget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if(response.success === true) {    
+            widget.clearTable();
+            widget.fillTable(response.data);
+            manage.updateUsersList(response.data);
+            manage.setMessage(response.success, response.message = 'Пользователь удален из списка избранных!');
+        } else {
+            manage.setMessage(response.success, response.message = 'Ошибка при удалении пользователя из списока избранных!');
+        };
+    });
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -6,10 +6,7 @@ userForm.loginFormCallback = data => {
     ApiConnector.login({login: data.login, password: data.password}, response => {
         if(response.success !== true) {
             userForm.setLoginErrorMessage(response.error);
-        };
-
-        if(response.success === true) {
-            console.log('Логин и пароль введены корректно, успешный вход в личный кабинет!')
+        } else {
             location.reload();
         };
     });
@@ -19,10 +16,7 @@ userForm.registerFormCallback = data => {
     ApiConnector.register({login: data.login, password: data.password}, response => {
         if(response.success !== true) {
             userForm.setRegisterErrorMessage(response.error);
-        };
-
-        if(response.success === true) {
-            console.log('Логин и пароль не найдены, успешная регистрация личного кабинета!')
+        } else {
             location.reload();
         };
     });
